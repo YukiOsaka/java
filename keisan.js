320fb3b8bf99d28ebbebd2bc1d276a74d9160b7a
Yongqi <21311056yo@tama.ac.jp>
2014-10-15 15:46:15 +0900

    keisan.js

---
+++ keisan.js
@@ -0,0 +1,16 @@
+class keisan {
+    public static void main(String args[])
+	{
+	   System.out.println("Hello,would!");
+	   // “ü—Í:‚Q‚Â‚Ì”’l‚ğ“ü—Í
+	   String input = new java.util.Scanner(System.in).nextLine();
+	   System.out.println(input);
+	   // ˆ—:‘«‚µZ
+	   // ˆ—:ˆø‚«Z
+	   // ˆ—:Š|‚¯Z
+	   // ˆ—:Š„‚èZ
+	   // ˆ—:—]‚è
+	   
+	   // ˆ—:
+	}
+}
