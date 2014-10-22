6237086e27317aa4dddb85af39385e009a2e27d6
Yongqi <21311056yo@tama.ac.jp>
2014-10-22 15:50:19 +0900

    keisan.java

--- keisan.java
+++ keisan.java
@@ -1,16 +1,26 @@
 class keisan {
     public static void main(String args[])
 	{
-	   System.out.println("Hello,would!");
-	   // “ü—Í:‚Q‚Â‚Ì”’l‚ğ“ü—Í
-	   String input = new java.util.Scanner(System.in).nextLine();
-	   System.out.println(input);
-	   // ˆ—:‘«‚µZ
-	   // ˆ—:ˆø‚«Z
-	   // ˆ—:Š|‚¯Z
-	   // ˆ—:Š„‚èZ
-	   // ˆ—:—]‚è
+	   // nyuu ryoku : 2tsu no suuchi wo nyuu ryoku
+	   int in0 = new java.util.Scanner(System.in).nextInt();
+       int in1 = new java.util.Scanner(System.in).nextInt();
+       System.out.println(in0+","+in1);
+	   // syori : tashizan
+	   int add = in0+in1;
+	   // syori : hikizan
+	   int sub = in0-in1;
+	   // syori : kakezan
+	   int mul = in0*in1;
+	   // syori : warizan
+	   int div = in0/in1;
+	   // syori : amari
+	   int rem = in0%in1;
 	   
-	   // ˆ—:
+	   // keisan kekka wo syutu ryoku
+	   System.out.println("tasizan:"+add);
+	   System.out.println("hikizan:"+sub);
+	   System.out.println("kakezan:"+mul);
+	   System.out.println("warizan:"+div);
+	   System.out.println("amari:"+rem);
 	}
 }
