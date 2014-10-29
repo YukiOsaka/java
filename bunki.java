90c95de8fb4ca7bcb71045150572603af499eb6c
Yongqi <21311056yo@tama.ac.jp>
2014-10-29 15:55:38 +0900

    bunki.javaの追加

---
+++ bunki.java
@@ -0,0 +1,22 @@
+class bunki {
+    public static void main(String args[])
+	{
+	   //　kotae
+	   int kotae = 0;
+	   // nyuu ryoku : suuchi1,kigou,suuchi2 no junban ni nyuu ryoku
+	   System.out.print("suuchi1=");
+	   int in0 = new java.util.Scanner(System.in).nextInt();
+	   System.out.print("tashizan(+)、hikizan(-)、kakezan(*)、warizan(/)、amari(%)no doreka=");
+	   //　String gata no hennsuu kigou ni, jouki no kigou wo nyuu ryoku
+	   String kigou = new java.util.Scanner(System.in).nextLine();
+	   System.out.print("suuchi2=");
+       int in1 = new java.util.Scanner(System.in).nextInt();
+       System.out.println(in0+","+in1);
+	   //　syori　：　kigou ni ouzite keisan
+	   
+	   //　syutsu ryoku　：　keisan kekka wo syutsu ryoku
+	   
+	   //　「suuchi1 keisankigou suuchi2 = kotae」
+	  　System.out.println("" + in0 + kigou + in1 + "=" + kotae);
+	}
+}
