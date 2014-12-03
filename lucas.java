f7117fe938e31afc49e8085e952a705d6abeaaab
Yongqi <21311056yo@tama.ac.jp>
2014-12-03 16:18:22 +0900

    lucas.java

--- lucas.java
+++ lucas.java
@@ -1,23 +1,31 @@
 class lucas {
-    public static void main(String args[])
+    
+	import java.util.BigInteger;
+	import java.io.*;
+	
+	public static void main(String args[])
 	{
 	    //lucas suuretsu(0~100 kou made sansyutsu site, goukei wo hyouzi)
-		BigInteger lucas = 0;
-		BigInteger lucas = new Big Integer("0");
+		BigInteger lucas = new BigInteger("0");
 		////hairetu no suuchi
 		BigInteger [] F = new BigInteger[101];
 		////0 koumoku no syokika
-		F[0] = 2;
+		F[0] = new BigInteger("2");
 		////1 koumoku no syokika
-		F[1] = 1;
+		F[1] = new BigInteger("1");
 		////2~10 kou no keisan
 		////2~10 made no ru-pu wo for bun de kaku
-		public static void main(String[] args); {
-           int max = 99;
-        for(int i=1;i<=max;i++){
-           System.out.println(i + " ==> "+ f(i));
+		//public static void main(String[] args); {
+        //   int max = 99;
+        for(int i=2; i<=100; i++){
+		// yaritai koto n = n ; one;
+           F[i] = F[i-1].add(F[i-2]);
+		   
+		   lucas = lucas + F[i];
+		   
+		   System.out.println(lucas);
         }
-        }
+        //}
 		
 		////for (BigInteger n=2 ; n<=100 ; n++) {
 		////     F[n] = F[n-1] + F[n-2];
@@ -25,7 +33,7 @@ class lucas {
 		////	 lucas = lucas + F[n];
 		////	 //tashita atai wo hyouzi
 		//// System.out.println(BigInteger.MAX_VALUE);
-		}
+	    //}
 		//// 1~10 kou made wo hyouji
 		////for (int n=1 ; n<=10 ; n++) {
 		    //// System.out.println(F[n]);
