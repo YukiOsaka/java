abb017aae93ea94699b6714c2eda41a45599c3ef
Yongqi <21311056yo@tama.ac.jp>
2014-12-10 16:09:54 +0900

    file.java

--- file.java
+++ file.java
@@ -1,28 +1,37 @@
+    import java.util.Random;
+    import java.io.*;
+
 class file {
-
-    import java.util.Rundam;
-	import java.io.*;
-
    public static void main(String args[])
    {
       //0~100 made no ransuu wo 100ko konma kugiri de fairu syutsu ryoku suru.
 	  //0~100 made no ransuu wo int gata ni 
 	 
 	 Random b = new Random();
-	 int a = b.nextInt(101);
-	 System.out.print(a);
+
 	 
 	try{ 
-	 File file = new File(tensu.txt);
-	 FileWriter fw = new FileWriter(file);
-	 BufferedWriter bw = new BufferedWriter(fw);
-	 PrintWriter pw = new PrintWriter(bw);
-	 pw.println(a);
-	 pw.close();
+	File file = new File("tensu.txt");
+    FileWriter fw = new FileWriter(file);
+    BufferedWriter bw = new BufferedWriter(fw);
+    PrintWriter pw = new PrintWriter(bw);
+	
+	//100ko wansuu wo fairu ni kakidasu
+	for(int i=1; i<=100; i++)
+	{
+	    //rannsuu wo seisei
+	    int a = b.nextInt(101);
+	    //fairu ni atai wo kakikomu
+	    System.out.print(a+",");
+		//System.out.print(a);
+		
+		//tasizan...
+		
+	}
+	 //fairu wo toziru
 	 }catch(Exception e)
 	 {
-	    System.out.printlm();
+	    System.out.println(e.toString());
 	 }
-	  
    }
 }
